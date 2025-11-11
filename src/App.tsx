5eae65279d768767cc946ef1919462577307de03
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2025-11-11 18:54:25 +0000

    added UI and logic for custom notification to display for 5 after message is generated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useState, useEffect} from "react";
 import TextInputComponent from "./components/TextInputComponent";
+import NotificationComponent from "./components/NotificationComponent";
 
 
 import GenerateMessage from "./logic/GenerateMessage";
@@ -8,6 +9,7 @@ import GenerateMessage from "./logic/GenerateMessage";
 function App() {
     const [lisaAgentSupportNAme, setLisaAgentSupportNAme] = useState("");
     const [lisaAgentName, setLisaAgentNAme] = useState("");
+    const [notificationVisible, setNotificationVisible] = useState(false);
 
 
     useEffect(() => {
@@ -20,37 +22,41 @@ function App() {
     }, [lisaAgentSupportNAme]);
 
     const handleButtonPress = async (agentDpt: string) => {
-        await GenerateMessage(agentDpt,lisaAgentName,lisaAgentSupportNAme);
-        alert("Message copied to clipboard");
+        await GenerateMessage(agentDpt, lisaAgentName, lisaAgentSupportNAme);
+        setNotificationVisible(true);
+        setTimeout(() => setNotificationVisible(false), 5000);
     }
-
     return (
-        <div className="formContainer">
-            <TextInputComponent htmlFor="agentInput" id="agentInput" label="Please enter your name:"
-                                textInputValue={lisaAgentSupportNAme} onChange={e => {
-                {
-                    setLisaAgentSupportNAme(e.target.value)
-                }
-            }}/>
-            <TextInputComponent htmlFor="lisaAgentInput" id="lisaAgentInput" label="Please enter the agent's name:"
-                                onChange={e => {
-                                    {
-                                        setLisaAgentNAme(e.target.value)
-                                    }
-                                }}/>
-            <button type="submit" onClick={async () => {
-                await handleButtonPress("Prepay");
-            }}>Prepay
-            </button>
-            <br/>
-            <br/>
-            <button type="submit"
-                    onClick={async() => {
-                        await handleButtonPress("Residential");
-                    }}
-            >Residential
-            </button>
-        </div>
+        <>
+            {notificationVisible && <NotificationComponent/>}
+            <div className="formContainer">
+
+                <TextInputComponent htmlFor="agentInput" id="agentInput" label="Please enter your name:"
+                                    textInputValue={lisaAgentSupportNAme} onChange={e => {
+                    {
+                        setLisaAgentSupportNAme(e.target.value)
+                    }
+                }}/>
+                <TextInputComponent htmlFor="lisaAgentInput" id="lisaAgentInput" label="Please enter the agent's name:"
+                                    onChange={e => {
+                                        {
+                                            setLisaAgentNAme(e.target.value)
+                                        }
+                                    }}/>
+                <button type="submit" onClick={async () => {
+                    await handleButtonPress("Prepay");
+                }}>Prepay
+                </button>
+                <br/>
+                <br/>
+                <button type="submit"
+                        onClick={async () => {
+                            await handleButtonPress("Residential");
+                        }}
+                >Residential
+                </button>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/NotificationComponent/index.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+const NotificationComponent = () => {
+    return (
+        <div className="notificationCtnr">
+            <p>
+                Successfully copied to Clipboard !
+            </p>
+        </div>
+    )
+}
+
+export default NotificationComponent
